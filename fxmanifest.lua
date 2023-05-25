0f56238cc49d609e1aa2be504d5af6b08d52a84b
Danilo <77410497+DaniloRds@users.noreply.github.com>
2023-05-25 16:19:46 -0300

    Update fxmanifest.lua
    Version: 1.8.1

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,20 +2,25 @@
 -- GitHub: https://github.com/DaniloRds
 -- Discord: https://discord.gg/pbT5wVp8e9
 -- Contact: unitystorefivem@gmail.com
--- Author: Tio Dan#9894
+-- Author: Danilo#2592
 -- Name: Airdrops 
--- Version: 1.0.0
--- Link: 
+-- Version: 1.8.1
 ----------------------- [ Unity ] -----------------------
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version 'adamant'
+game 'gta5'
+author 'Danilo#2592'
+description 'Unity FiveM | discord.gg/pbT5wVp8e9'
 
-dependency "vrp"
+-- Versão VRPEX
 
-server_scripts {
-  '@vrp/lib/utils.lua',
-  'server.lua'
+client_scripts {
+    "@vrp/lib/utils.lua",
+    'config.lua',
+    'client.lua'
+
 }
-
-client_scripts{
-  'client.lua'
+server_scripts {
+    "@vrp/lib/utils.lua",
+    'config.lua',
+    'server.lua'
 }
